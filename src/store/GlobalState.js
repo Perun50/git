f2e7df9af74db71f8823bf8576a03587be6c38a9
Perun50 <basilisk233@gmail.com>
2022-03-21 10:39:01 +0100

    commit on "main"

--- src/store/GlobalState.js
+++ src/store/GlobalState.js
@@ -1,42 +1,61 @@
 import React, { useReducer, createContext } from "react";
 
 const initialState = {
-  transactions : [
+  transactions: [
     { transaction: "heat", amount: "20", id: 324 },
     { transaction: "bonus", amount: "100", id: 5678 },
   ],
 };
 
-function reducer(state, action){
-    switch(action.type){
-        case "DELETE":
-            return{
-                ...state,
-                transactions: state.transactions.filter(item=> item.id !== action.payload)
-            }
-        default:
-            return state.transactions
-    }
+function reducer(state, action) {
+  switch (action.type) {
+    case "DELETE_TRANSACTION":
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction.id !== action.payload
+        ),
+      };
+    case "ADD_TRANSACTION":
+      return {
+        ...state,
+        transactions: [action.payload, ...state.transactions],
+      };
+    default:
+      return state;
+  }
 }
 
-export  const GlobalContext = createContext(initialState);
+export const GlobalContext = createContext(initialState);
 
-function GlobalState({children}) {
-    const [state, dispatch] = useReducer(reducer, initialState)
-     
-    function deleteTransaction(id){
-        dispatch ({
-            type: "DELETE",
-            payload: id
-        })
-    }
+function GlobalState({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  return <GlobalContext.Provider value={{
-      transactions: state.transactions,
-      deleteTransaction,
-  }}>
-    {children}
-  </GlobalContext.Provider>
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE",
+      payload: id,
+    });
+  }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  }
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
 }
 
 export default GlobalState;
